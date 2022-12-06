578de7929bf73620a50f1e20e62af217ab666b8b
PeterTian96 <89152255+PeterTian96@users.noreply.github.com>
2022-12-06 10:09:57 -0600

    final update

--- TransRater/data_clean.py
+++ TransRater/data_clean.py
@@ -18,10 +18,11 @@ class Data_cleaning:
   This class is used to clean the MT_data and DAT_data, and combine the two datasets as the final clean dataset.
   """
 
-  def __init__ (self, MT_data, DAT_data, zip_data):
+  def __init__ (self, MT_data, DAT_data, zip_data, mapping):
     self.mt = MT_data
     self.dat = DAT_data
     self.zip_data = zip_data
+    self.mapping = mapping
     self.us_list = [ 'AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA',
            'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME',
            'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM',
@@ -29,25 +30,26 @@ class Data_cleaning:
            'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY']
     self.canada_list = ['AB','BC','MB','NB','NL','NT','NS','NU','ON','PE','QC','SK','YT']
     self.actual_mode_list = ['LTL','TL','INTERMODAL']
-    self.required_column = ['CUSTOMER', 'DISTANCE', 'CASES', 'WEIGHT', 'VOLUME', 
-        'ACTUAL CARRIER', 'ACTUAL MODE','ACTUAL EQUIP', 'PU_APPT',
-        'DL_APPT', 'SPOT_AVG_LINEHAUL_RATE', 'SPOT_LOW_LINEHAUL_RATE',
+    self.mt_required_column = ['CUSTOMER', 'DISTANCE', 'CASES', 'WEIGHT', 'VOLUME', 
+        'ACTUAL CARRIER', 'ACTUAL MODE','ACTUAL EQUIP', 'PU_APPT', 'DL_APPT', 'LINEHAUL COSTS']
+    self.dat_required_column = ['SPOT_AVG_LINEHAUL_RATE', 'SPOT_LOW_LINEHAUL_RATE',
        'SPOT_HIGH_LINEHAUL_RATE', 'SPOT_FUEL_SURCHARGE', 'SPOT_TIME_FRAME',
        'SPOT_YOUR_OWN_AVG_LINEHAUL_RATE', 'CONTRACT_AVG_LINEHAUL_RATE', 
        'CONTRACT_LOW_LINEHAUL_RATE', 'CONTRACT_HIGH_LINEHAUL_RATE', 'CONTRACT_FUEL_SURCHARGE',
        'CONTRACT_AVG_ACCESSORIAL_EXCLUDES_FUEL', 'CONTRACT_TIME_FRAME',
-       'CONTRACT_YOUR_OWN_AVG_LINEHAUL_RATE', 'ORIGIN_LAT', 'ORIGIN_LNG', 'DEST_LAT', 'DEST_LNG', 'LINEHAUL COSTS']
+       'CONTRACT_YOUR_OWN_AVG_LINEHAUL_RATE']
     self.dummy_column = ['CUSTOMER', 'ACTUAL CARRIER', 'ACTUAL MODE', 'ACTUAL EQUIP']
 
   def clean_mt_data(self):
     # first, drop the data that has wrong origin state or destination state
-    self.mt = self.mt[self.mt['ORIGIN STATE'].isin(self.us_list + self.canada_list) &\
-                      self.mt['DEST STATE'].isin(self.us_list + self.canada_list)]
+    for i in self.mt_required_column:
+      if i not in self.mt.columns:
+        self.mt = pd.concat([self.mt, pd.DataFrame(columns=[i])], sort=False)
+        self.mt[i] = self.mt[i].replace(np.nan,0)
 
     self.mt['ID_OR_OPTIONAL_FIELD'] = self.mt['ORIGIN ZIP'].astype(str).str[:3] + '_' + self.mt['DEST ZIP'].astype(str).str[:3] 
     self.mt['ID_OR_OPTIONAL_FIELD'] = self.mt['ID_OR_OPTIONAL_FIELD'].astype(str)
 
-    self.mt = self.mt.drop(['Unnamed: 0.1','Unnamed: 0'], axis = 1)
     self.mt = self.mt[pd.notnull(self.mt['CUSTOMER'])]
 
     # Drop the row if na is found in the MT_data['DISTANCE']
@@ -59,11 +61,7 @@ class Data_cleaning:
     # Drop the row if na is found in the MT_data['VOLUME']
     self.mt = self.mt[pd.notnull(self.mt['VOLUME'])]
 
-    # Drop the row if na is found in the MT_data['ORIGIN CITY']
-    self.mt = self.mt[pd.notnull(self.mt['ORIGIN CITY'])]
-
     self.mt = self.mt[pd.notnull(self.mt['LINEHAUL COSTS'])]
-    self.mt = self.mt[pd.notnull(self.mt['ORIGIN STATE'])]
     self.mt = self.mt.reset_index(drop=True)
     self.mt = self.mt[pd.notnull(self.mt['ORIGIN ZIP'])]
     self.mt = self.mt.reset_index(drop=True)
@@ -81,24 +79,6 @@ class Data_cleaning:
     # Remove any nan cell in LINEHAUL COSTS
     self.mt = self.mt[pd.notnull(self.mt['LINEHAUL COSTS'])]
 
-    # Remove any nan cell in FUEL COSTS
-    self.mt = self.mt[pd.notnull(self.mt['FUEL COSTS'])]
-
-    # Remove any nan cell in ACC. COSTS
-    self.mt = self.mt[pd.notnull(self.mt['ACC. COSTS'])]
-
-    # Remove any nan cell in total ACTUAL COST
-    self.mt = self.mt[pd.notnull(self.mt['TOTAL ACTUAL COST'])]
-
-    # Add a LINEHAUL UNIT COSTS to the dataframe
-    self.mt['LINEHAUL UNIT COSTS'] = self.mt['LINEHAUL COSTS']/self.mt['DISTANCE']
-
-    # Drop the row if na is found in the MT_data['DEST CITY']
-    self.mt = self.mt[pd.notnull(self.mt['DEST CITY'])]
-
-    # Only conisder shipment has dest zip code in USA and Canada
-    # Drop the row if ns is found in the MT_data['ORIGIN STATE']
-    self.mt = self.mt[pd.notnull(self.mt['DEST STATE'])]
     self.mt = self.mt.reset_index(drop=True)
 
     self.mt = self.mt[pd.notnull(self.mt['DEST ZIP'])]
@@ -115,19 +95,21 @@ class Data_cleaning:
     self.mt['File_paried_data'] = self.mt['File_paried_data'].dt.strftime('%Y-%m-%d')
 
     # merge the zip data into MT_data via origin information and destination information, respectively
-    self.mt = pd.merge(self.mt, self.zip_data, how = 'left', left_on = ['ORIGIN ZIP', 'ORIGIN STATE'], right_on = ['zipcode', 'state'])
-    self.mt = pd.merge(self.mt, self.zip_data, how = 'left', left_on = ['DEST ZIP', 'DEST STATE'], right_on = ['zipcode', 'state'])
+    self.mt = pd.merge(self.mt, self.zip_data, how = 'left', left_on = ['ORIGIN ZIP'], right_on = ['zipcode'])
+    self.mt = pd.merge(self.mt, self.zip_data, how = 'left', left_on = ['DEST ZIP'], right_on = ['zipcode'])
     # delete unwanted columns and change the column names
     self.mt = self.mt.drop(['zipcode_x', 'state_x', 'zipcode_y', 'state_y'], axis = 1)
     self.mt = self.mt.rename(columns={'lng_x':'ORIGIN_LNG', 'lat_x':'ORIGIN_LAT', 'lng_y':'DEST_LNG', 'lat_y':'DEST_LAT'})
     # delete data that doesn't match
     self.mt = self.mt.dropna(subset = ['ORIGIN_LNG', 'ORIGIN_LAT', 'DEST_LNG', 'DEST_LAT'])
+    self.mt = self.mt[self.mt_required_column + ['File_paried_data', 'ID_OR_OPTIONAL_FIELD', 'ORIGIN_LAT', 'ORIGIN_LNG', 'DEST_LAT', 'DEST_LNG']]
     return self.mt
 
   def clean_dat_data(self):
-    # first, drop the data that has wrong origin state or destination state
-    self.dat = self.dat[self.dat['ORIG_STATE'].isin(self.us_list + self.canada_list) &\
-                      self.dat['DEST_STATE'].isin(self.us_list + self.canada_list)]
+    for i in self.dat_required_column:
+      if i not in self.dat.columns:
+        self.dat = pd.concat([self.dat, pd.DataFrame(columns=[i])], sort=False)
+        self.dat[i] = self.dat[i].replace(np.nan,0)
 
     # Set ID_OR_OPTIONAL_FIELD as an objective column
     self.dat['ID_OR_OPTIONAL_FIELD'] = self.dat['ID_OR_OPTIONAL_FIELD'].astype(str)
@@ -158,7 +140,10 @@ class Data_cleaning:
 
     # Drop the row if na is found in the DAT_data['CONTRACT_TIME_FRAME']
     self.dat = self.dat[pd.notnull(self.dat['CONTRACT_TIME_FRAME'])]
+    self.dat['SPOT_YOUR_OWN_AVG_LINEHAUL_RATE'] = self.dat['SPOT_YOUR_OWN_AVG_LINEHAUL_RATE'].replace(np.nan,0)
+    self.dat['CONTRACT_YOUR_OWN_AVG_LINEHAUL_RATE'] = self.dat['CONTRACT_YOUR_OWN_AVG_LINEHAUL_RATE'].replace(np.nan,0)
     self.dat = self.dat.reset_index(drop = True)
+    self.dat = self.dat[self.dat_required_column + ['ID_OR_OPTIONAL_FIELD', 'File_data']]
     return self.dat
   
   def final_data(self):
@@ -170,7 +155,7 @@ class Data_cleaning:
     merge_df_remove_NA = merge_df_remove_NA.reset_index(drop=True)
 
     merge_df_remove_NA = merge_df_remove_NA[pd.to_datetime(merge_df_remove_NA['File_paried_data']) <= pd.to_datetime(merge_df_remove_NA['File_data'])]
-    merge_df_remove_NA = merge_df_remove_NA[self.required_column]
+    merge_df_remove_NA = merge_df_remove_NA.drop(['File_paried_data', 'ID_OR_OPTIONAL_FIELD', 'File_data'], axis = 1)
 
     for col in self.dummy_column:
       temp_col = pd.get_dummies(merge_df_remove_NA[col], prefix = col)
@@ -180,8 +165,24 @@ class Data_cleaning:
       temp_col = (merge_df_remove_NA[col].astype('datetime64[D]') - merge_df_remove_NA[col].astype('datetime64[Y]'))/ np.timedelta64(1, 'D')
       merge_df_remove_NA.drop(col, axis=1, inplace=True)
       merge_df_remove_NA = pd.concat([merge_df_remove_NA, temp_col],axis=1)
-    merge_df_remove_NA = merge_df_remove_NA.astype('float64')
+    for col in self.mapping['ACTUAL CARRIER']:
+      if col not in merge_df_remove_NA.columns:
+        merge_df_remove_NA = pd.concat([ merge_df_remove_NA, pd.DataFrame(columns=[col])], axis = 1, sort=False)
+        merge_df_remove_NA[col] = merge_df_remove_NA[col].replace(np.nan,0)
+    for col in self.mapping['ACTUAL EQUIP']:
+      if col not in merge_df_remove_NA.columns:
+        merge_df_remove_NA = pd.concat([ merge_df_remove_NA, pd.DataFrame(columns=[col])], axis = 1, sort=False)
+        merge_df_remove_NA[col] = merge_df_remove_NA[col].replace(np.nan,0)
+    for col in self.mapping['ACTUAL MODE']:
+      if col not in merge_df_remove_NA.columns:
+        merge_df_remove_NA = pd.concat([ merge_df_remove_NA, pd.DataFrame(columns=[col])], axis = 1, sort=False)
+        merge_df_remove_NA[col] = merge_df_remove_NA[col].replace(np.nan,0)
+    for col in self.mapping['CUSTOMER']:
+      if col not in merge_df_remove_NA.columns:
+        merge_df_remove_NA = pd.concat([ merge_df_remove_NA, pd.DataFrame(columns=[col])], axis = 1, sort=False)
+        merge_df_remove_NA[col] = merge_df_remove_NA[col].replace(np.nan,0)
 
+    merge_df_remove_NA = merge_df_remove_NA.astype('float64')
 
     self.cleaned_df = merge_df_remove_NA
     self.LTL_Cleaned_Data = self.cleaned_df[self.cleaned_df['ACTUAL MODE_LTL'] == 1.0]
---
+++ TransRater/putback.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 29 20:39:12 2022
+
+@author: petertian
+"""
+
+import pandas as pd
+
+path = '/Users/petertian/Desktop/文件/Vanderbilt/semester 3/Machine Learning 2/ML_Project_Transpotation_Rate/TransRater/sample data'
+data = pd.read_csv(path + '/1.csv')
